208954f969a6499550921dc7a03b7829c48c076b
Tegar Ferdigantara <tegarferdigantara007@gmail.com>
2026-01-16 18:51:11 +0700

    rohanauthcontroller path changed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RohanAuthController;
+use App\Http\Controllers\Rohan\RohanAuthController;
 
 /*
 |--------------------------------------------------------------------------
